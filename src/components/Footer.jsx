7127a66e0c6495ff541d7a34fb85f8fec0ff3218
sathwikareddy12 <sathwika.jali@gmail.com>
2025-05-28 23:38:26 +0530

    third commit of portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -48,7 +48,7 @@ const Footer = () => {
             </a>
           </div>
 
-          <p className="mt-6 text-sm text-gray-500">© {new Date().getFullYear()} Built with ❤️ by jali sathwika reddy</p>
+          <p className="mt-6 text-sm text-gray-500">© {new Date().getFullYear()} Engineered by jali sathwika reddy</p>
         </div>
       </div>
     </footer>
